ef0ad82cf14c732c8c8ad06624907c4bbcba2015
VaporSiriz <61943747+VaporSiriz@users.noreply.github.com>
2023-01-09 10:08:27 +0900

    Time: 30 ms (99.94%), Space: 16.3 MB (92.88%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,44 +6,19 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def check_parent(parents, node_val):
-            flag = True
-            for parent in parents:
-                parent_val, direction = parent
-                print(f"{direction} - {node_val} {'<?' if direction == 'l' else '>?'} {parent_val}")
-                flag = node_val < parent_val if direction == 'l' else node_val > parent_val
-                if not flag:
-                    break
-            return flag
+        stack, prev = [], -math.inf
 
-
-        queue = []
-        if root.left is not None:
-            if root.left.val < root.val:
-                queue.append((root.left, 'l', [(root.val, 'l')]))
-            else:
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            # If next element in inorder traversal
+            # is smaller than the previous one
+            # that's not BST.
+            if root.val <= prev:
                 return False
-        if root.right is not None:
-            if root.right.val > root.val:
-                queue.append((root.right, 'r', [(root.val, 'r')]))
-            else:
-                return False
+            prev = root.val
+            root = root.right
 
-        while len(queue)!=0:
-            node, direction, parents = queue.pop()
-            left: TreeNode = node.left
-            right: TreeNode = node.right
-            print(f"{node.val}, {direction}, {parents}")
-            if left is not None:
-                print(f"left : {left.val} <? {node.val}")
-                if left.val < node.val and check_parent(parents, left.val):
-                    queue.append((left, 'l', parents+[(node.val, 'l')]))
-                else:
-                    return False
-            if right is not None:
-                print(f"right : {right.val} >? {node.val}")
-                if right.val > node.val and check_parent(parents, right.val):
-                    queue.append((right, 'r', parents+[(node.val, 'r')]))
-                else:
-                    return False
         return True
