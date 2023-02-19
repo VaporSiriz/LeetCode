a446b7d63ccd3c872177239bba09a86f685da6e3
VaporSiriz <61943747+VaporSiriz@users.noreply.github.com>
2023-02-20 08:40:12 +0900

    Time: 3284 ms (7.17%), Space: 16.5 MB (7.59%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -7,13 +7,22 @@ class Solution:
         100
         
         """
-        sqrts = []
-        for i in range(0, 65536+1):
-            sqrts.append(i*i)
-        
-        for index, sqrt in enumerate(sqrts):
-            #print(f"{index}, {x}>?{sqrt}")
-            if x<sqrt:
-                #print(sqrts[index-1])
-                return index-1
+        sqrts = [i*i for i in range(0, 65536+1)]
+        start = 0
+        end = len(sqrts)
+        # 큰 것들 중에 가장 작은 수 찾기
+        # 작은 숫자를 찾았다 > 오른쪽으로 쭉?, 큰 숫자를 찾았다 > 왼쪽으로 쭉? 겨우 절반 줄임
+        # 끝까지 가자.
+        while start<=end:
+            mid = start + int((end-start)/2)
+            #print(f"{mid} - {sqrts[mid]} !=? x")
+            if sqrts[mid] == x:
+                return mid
+            if sqrts[mid] < x:
+                start = mid+1
+            else:
+                end = mid-1
+        #print(f"start: {start}, end:{end}")
+        return end
+            
         
