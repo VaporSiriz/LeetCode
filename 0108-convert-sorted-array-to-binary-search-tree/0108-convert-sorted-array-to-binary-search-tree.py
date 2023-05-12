e5aedc69914426f46113fba660a734303f6da097
VaporSiriz <61943747+VaporSiriz@users.noreply.github.com>
2023-05-12 09:37:40 +0900

    Time: 99 ms (5.41%), Space: 18.5 MB (7.38%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -6,25 +6,17 @@
 #         self.right = right
 class Solution:
     
-    def buildBST(self, node: Optional[TreeNode], nums: List[int], direction: str):
+    def buildBST(self, node: Optional[TreeNode], nums: List[int]):
         if len(nums)==0:
             return None
-        #print(f"node : {node} - nums : {nums}, {direction}")
-        if len(nums)==1:
-            return TreeNode(val=nums[0])
-        start = 0
-        end = len(nums)
-        middle = (end-start)//2
+        middle = len(nums)//2
+        #print(f"middle : {middle}, nums : {nums}")
         node = TreeNode(val=nums[middle])
-        #print(f"{start}-{middle}-{end}")
-        #print(f"l : {start}:{end if end==middle else middle}, {nums[start:middle+1 if end==middle else middle]}")
-        node.left = self.buildBST(node, nums[start:middle+1 if end==middle else middle], 'l')
-        
-        #print(f"r : {middle+1}:{end}, {nums[middle+1:end]}")
-        node.right = self.buildBST(node, nums[middle+1:end], 'r')
+        node.left = self.buildBST(node, nums[:middle])
+        node.right = self.buildBST(node, nums[middle+1:])
         return node
     
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        root = self.buildBST(node=None, nums=nums, direction='')
+        root = self.buildBST(node=None, nums=nums)
         print(f"root : {root}")
         return root
